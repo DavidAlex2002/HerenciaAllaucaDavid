36b729a435dc1aab3d59207de3086e87f08da0de
davidallauca <daallauca@espe.edu.ec>
2021-12-22 10:54:15 -0500

    HerenciaAllaucaDavid

---
+++ Herencia/src/HerenciaJava/Seleccion.java
@@ -0,0 +1,63 @@
+package HerenciaJava;
+
+public class Seleccion {
+
+	protected int id;
+
+	protected String Nombre;
+
+	protected String Apellidos;
+
+	protected int Edad;
+
+	public Seleccion() {
+	}
+
+	public Seleccion(int id, String nombre, String apellidos, int edad) {
+		this.id = id;
+		this.Nombre = nombre;
+		this.Apellidos = apellidos;
+		this.Edad = edad;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return Nombre;
+	}
+
+	public void setNombre(String nombre) {
+		Nombre = nombre;
+	}
+
+	public String getApellidos() {
+		return Apellidos;
+	}
+
+	public void setApellidos(String apellidos) {
+		Apellidos = apellidos;
+	}
+
+	public int getEdad() {
+		return Edad;
+	}
+
+	public void setEdad(int edad) {
+		Edad = edad;
+	}
+
+	public void Concentrarse() {
+		System.out.println("Concentrarse");
+	}
+
+	public void Viajar() {
+		System.out.println("Viajar");
+	}
+
+}
